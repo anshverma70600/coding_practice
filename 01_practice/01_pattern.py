c0735999015ba5a8bc60a8388cbdd9ca9760060d
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-03-03 15:19:26 +0000

    pattern

--- 01_practice/01_pattern.py
+++ 01_practice/01_pattern.py
@@ -6,12 +6,13 @@
 #     print("")
 
 
+
 # right angled triangle 
 n = 10
-for i in range(n):
-    for j in range(i):
-        print("*",end=' ')
-    print('')
+# for i in range(n):
+#     for j in range(i):
+#         print("*",end=' ')
+#     print('')
 for i in range(n):
     for j in range(n,0,-1):
         print("*",end=' ')
